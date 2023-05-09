5db3854cba0303c41f323f392315cd1201404759
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-05-09 17:12:35 -0300

    Revert "feat: resolver req3 trad: req 1,2,3"
    This reverts commit 2fb77d78079fccc805b92ecc3cd9ac7ffe0ab005.

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -5,16 +5,23 @@ from .simple_report import SimpleReport
 class CompleteReport(SimpleReport):
     @staticmethod
     def generate(dados):
-        simplArmaz = SimpleReport.generate(dados)
+        data_fabricacao_mais_antiga = min(
+            d["data_de_fabricacao"] for d in dados
+        )
+        data_validade_mais_proxima = max(d["data_de_validade"] for d in dados)
+
         empresas = [d["nome_da_empresa"] for d in dados]
         produtos = Counter(empresas)
-
+        empresa_com_mais_produtos = produtos.most_common(1)[0][0]
         produtos_estocados = produtos.most_common(None)
-        linhas_do_relatorio = (
-            simplArmaz + "\nProdutos estocados por empresa:\n"
-        )
 
+        linhas_do_relatorio = [
+            f"Data de fabricação mais antiga: {data_fabricacao_mais_antiga}\n",
+            f"Data de validade mais próxima: {data_validade_mais_proxima}\n",
+            f"Empresa com mais produtos: {empresa_com_mais_produtos}\n",
+            "Produtos estocados por empresa:\n",
+        ]
         for empresa, num_produtos in produtos_estocados:
-            linhas_do_relatorio += f"- {empresa}: {num_produtos}\n"
+            linhas_do_relatorio.append(f"- {empresa}: {num_produtos}\n")
 
         return "".join(linhas_do_relatorio)
--- tests/product_report/test_product_report.py
+++
@@ -1,5 +0,0 @@
-# from inventory_report.inventory.product import Product
-
-
-def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
