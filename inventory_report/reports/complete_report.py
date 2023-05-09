fd7b9145ece2d024741c65107a32228542f435b8
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-05-09 15:32:37 -0300

    trad req3 and

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -4,22 +4,24 @@ from .simple_report import SimpleReport
 
 class CompleteReport(SimpleReport):
     @staticmethod
-    def generate(data):
-        min_date = min(d["data_de_fabricacao"] for d in data)
-        max_date = max(d["data_de_validade"] for d in data)
+    def generate(dados):
+        data_fabricacao_mais_antiga = min(
+            d["data_de_fabricacao"] for d in dados
+        )
+        data_validade_mais_proxima = max(d["data_de_validade"] for d in dados)
 
-        empresas = [d["nome_da_empresa"] for d in data]
-        products = Counter(empresas)
-        max_products = products.most_common(1)[0][0]
-        estoq_products = products.most_common(None)
+        empresas = [d["nome_da_empresa"] for d in dados]
+        produtos = Counter(empresas)
+        empresa_com_mais_produtos = produtos.most_common(1)[0][0]
+        produtos_estocados = produtos.most_common(None)
 
-        report_lines = [
-            f"Data de fabricação mais antiga: {min_date}\n",
-            f"Data de validade mais próxima: {max_date}\n",
-            f"Empresa com mais produtos: {max_products}\n",
+        linhas_do_relatorio = [
+            f"Data de fabricação mais antiga: {data_fabricacao_mais_antiga}\n",
+            f"Data de validade mais próxima: {data_validade_mais_proxima}\n",
+            f"Empresa com mais produtos: {empresa_com_mais_produtos}\n",
             "Produtos estocados por empresa:\n",
         ]
-        for empresa, num_products in estoq_products:
-            report_lines.append(f"- {empresa}: {num_products}\n")
+        for empresa, num_produtos in produtos_estocados:
+            linhas_do_relatorio.append(f"- {empresa}: {num_produtos}\n")
 
-        return "".join(report_lines)
+        return "".join(linhas_do_relatorio)
