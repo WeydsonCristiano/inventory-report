b695c0bbf19b9b81f5341ada787a32d9f2b9a313
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-05-09 13:27:00 -0300

    feat: req3 fail

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,25 @@
-class CompleteReport:
-    pass
+from collections import Counter
+from .simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(data):
+        min_date = min(d["data_de_fabricacao"] for d in data)
+        max_date = max(d["data_de_validade"] for d in data)
+
+        empresas = [d["nome_da_empresa"] for d in data]
+        products = Counter(empresas)
+        max_products = products.most_common(1)[0][0]
+        sorted_company_products = products.most_common(None)
+
+        report_lines = [
+            f"Data de fabricação mais antiga: {min_date}",
+            f"Data de validade mais próxima: {max_date}",
+            f"Empresa com mais produtos: {max_products}",
+            "Produtos estocados por empresa:",
+        ]
+        for empresa, num_products in sorted_company_products:
+            report_lines.append(f"- {empresa}: {num_products}")
+
+        return "\n".join(report_lines)
